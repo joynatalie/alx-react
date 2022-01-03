3dd1fd203ee407d3abe52084324b62b21d0de5ad
Renzo Perez <renzin@gmail.com>
2022-01-03 17:14:58 -0500

    adding asset rule

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,26 +1,30 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
-	performance: {
+  performance: {
     maxAssetSize: 1000000,
     maxEntrypointSize: 1000000,
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
   },
   module: {
     rules: [
       {
-				test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
 			{
 				type: "asset",
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 			},
-    ],
+      
+    ]
   },
-  mode: 'production',
 };
