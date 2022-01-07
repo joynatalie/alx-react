748231631b634f077dc15a250bebb25dc83b3aba
derpmagician <renzin@gmail.com>
2022-01-07 10:54:29 -0500

    adding files

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,28 +1,28 @@
-import React from "react";
-import "./App.css";
-import holberton_logo from "../assets/holberton_logo.jpg";
-import { getFullYear, getFooterCopy } from "../utils/utils";
+import React from 'react';
+import './App.css';
+import logo from '../assets/holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
     <>
-      <div className="App-header">
-        <img src={holberton_logo} />
+      <header className='App-header'>
+        <img src={logo} alt='logo' />
         <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
+      </header>
+      <main role='main' className='App-body'>
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-        <button>OK</button>
-      </div>
-      <div className="App-footer">
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='button'>OK</button>
+      </main>
+      <footer className='App-footer'>
         <p>
           Copyright {getFullYear()} - {getFooterCopy(true)}
         </p>
-      </div>
+      </footer>
     </>
   );
 }
--- 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,31 +1,38 @@
-import React from "react";
-import "./Notifications.css";
-import { getLatestNotification } from "../utils/utils";
-import closeIcon from "../assets/close-icon.png";
+import React from 'react';
+import './Notifications.css';
+import close_icon from '../assets/close-icon.png';
+import { getLatestNotification } from '../utils/utils';
 
 const Notifications = () => {
   return (
-    <div className="Notifications">
-      <button
-        style={{
-          background: "transparent",
-          border: "none",
-          position: "absolute",
-          right: 20,
-        }}
-        aria-label="close"
-      >
-        <img src={closeIcon} alt="close-icon" />
-      </button>
+    <div className='Notifications'>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li
-          data-priority="urgent"
-          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-        ></li>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li data-priority='urgent'>
+          <div
+            dangerouslySetInnerHTML={{ __html: `${getLatestNotification()}` }}
+          ></div>
+        </li>
       </ul>
+      <button
+        type='button'
+        aria-label='Close'
+        onClick={() => console.log('Close button has been clicked')}
+        style={{
+          display: 'inline-block',
+          position: 'absolute',
+          top: '16px',
+          right: '16px',
+          background: 0,
+          border: 0,
+          outline: 'none',
+          cursor: 'pointer',
+        }}
+      >
+        <img src={close_icon} alt='' style={{ width: '8px', height: '8px' }} />
+      </button>
     </div>
   );
 };
